de74f3daa938eb87d1f4ec2e60956c4a0a9dabcf
ihavalyova <69295784+ihavalyova@users.noreply.github.com>
2020-10-29 10:11:48 +0200

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Author:  Ilvie Havalyova
+# Contact: havaliova@gmail.com
+
+def run_setup():
+  pass
+
+def main():
+    run_setup()
+
+if __name__ == '__main__':
+    main()
