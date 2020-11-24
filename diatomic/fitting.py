derivatives changed; bug fixes and code refactoring

diatomic/fitting.py
@@ -1,10 +1,11 @@
-import os
 import io
-import math
+from os.path import join as _join
+from math import sqrt as _sqrt
+from logging import error as _error, warning as _warning
 import numpy as np
 import scipy as sp
-from .molecule_data import Channel, Coupling
-from .utils import Utils, C_hartree
+from molecule_data import Channel, Coupling
+from utils import Utils, C_hartree
 # from numba import njit, guvectorize, int64, float64
 
 try:
@@ -18,10 +19,10 @@ __all__ = ['Fitting']
 
 class Fitting:
 
-    def __init__(self, ml, progress=False):
+    def __init__(self, ml, progress_full=False):
 
         self.ml = ml
-        self.progress = progress
+        self.progress = progress_full
 
         try:
             Utils.createBackup(Coupling.cpl_file)
@@ -52,7 +53,7 @@ class Fitting:
         stats = self.ml.calculate_eigenvalues(ypar=ypar)
 
         # Uncomment this if intermediate results are needed for debugging
-        print(f'Chi Square = {stats[0]:<18.8f} |RMS = {stats[1]:<15.8f}cm-1')
+        # print(f'Chi Square = {stats[0]:<18.8f} |RMS = {stats[1]:<15.8f}cm-1')
 
         return self.ml.out_data, stats
 
@@ -148,7 +149,6 @@ class Fitting:
 
     def run_svd(self, niter=1, deriv='n', tol=0.1, lapack_driver='gelsd',
                 step_size=1.0e-4, is_weighted=False, regular=0.0):
-
         """SVD algorithm for nonliner least squares fitting.Solves Ax = b using
         Singular Value Decomposition (SVD). Starting from some trial set of
         parameters on each iteration the linear system Ax=b for the corrections
@@ -169,23 +169,20 @@ class Fitting:
             restart (bool, optional): Not yet implemented. Defaults to False.
         """
 
+        deriv = deriv.lower()
+        if deriv == 'a':
+            self.check_derivative()
+
         # (0) get the initial parameters
         ypar, yfixed, yreg, ylam = self._get_initial_parameters()
-        is_failed = False
+        eps = 1.0e-3
 
         for it in range(1, niter + 1):
 
-            self.progress_str = io.StringIO()
-            self.progress_str.write(
-                f'\n{18*"- "}Iteration {it} {18*"- "} \n'
-                f'\nTOL{"":>23}= {tol:.1e}'
-            )
-
-            # (1) get initially calculated energies on every new iteration
+            # (1) get the updated energies from the previous iteration
             out_data, stats = self._get_eigenvalues(
                 ypar, is_weighted=is_weighted
             )
-
             ycal = out_data[:, 7].astype(np.float64) / C_hartree
             ydel = -out_data[:, 9].astype(np.float64) / C_hartree
             yvar = out_data[:, 10].astype(np.float64) / C_hartree
@@ -200,15 +197,18 @@ class Fitting:
                 c = cmult * regular
                 chi2r = np.sum(np.square(c))
                 chi2_best = chi2_best + chi2r
-                rms_best = rms_best + math.sqrt(chi2r)
+                rms_best = rms_best + _sqrt(chi2r)
 
-            self.progress_str.write(
-                f'\nChi square initial{"":>8}= {chi2_best:.8f}'
-                f'\nRMS initial{"":>15}= {rms_best:.8f} cm-1\n'
+            info_init = (
+                f'{14*"- "}Iteration {it} {14*"- "}\nTOL = {tol:.1e}\n'
+                f'Chi square = {chi2_best:.6f} | RMS = {rms_best:.6f} cm-1\n'
             )
+            print(info_init)
+            self.progress_str = io.StringIO()
+            self.progress_str.write(info_init)
 
-            # (2) generate the matrix A - the matrix containing the
-            # derivatives of the energies with respect to the parameters
+            # (2) generate the matrix containing the derivatives of the
+            # energies with respect to the parameters
 
             if deriv == 'n':
                 dydp = self._generate_numerical_derivatives(
@@ -217,28 +217,27 @@ class Fitting:
                 )
                 # np.savetxt('dydp_numerical.dat', dydp, fmt='%.6e')
             else:
-                # the quantum numbers are aranged as follows:
                 # v, J, par, iso, state
                 qnums = np.column_stack((
                     out_data[:, 0], out_data[:, 1], out_data[:, 5],
                     (out_data[:, 6] % self.ml.nisotopes)+1, out_data[:, -1]
                 ))
                 dydp = self._generate_analytical_derivatives(
-                    ypar, yfixed, qnums, is_weighted=is_weighted
+                    ypar, yfixed, qnums
                 )
                 # np.savetxt('dydp_analytical.dat', dydp, fmt='%.6e')
 
+            # (3) calculate the data matrix A from derivatives matrix
+            # then calculate the rhs vector b
             A = (dydp.T / yvar).T
-
-            # (3) calculate the rhs vector b
             b = ydel / yvar
 
             # extend the system Ax=b with new one: Bx=c
             if regular != 0.0:
                 # B = (np.eye(ypar.shape[0], ypar.shape[0]) / ylam) * regular
-                idm = np.eye(ypar.shape[0], ypar.shape[0])
+                identm = np.eye(ypar.shape[0], ypar.shape[0])
                 bmult = np.divide(
-                    idm, ylam, out=np.zeros_like(idm), where=ylam != 0
+                    identm, ylam, out=np.zeros_like(identm), where=ylam != 0
                 )
                 B = bmult * regular
                 A = np.vstack((A, B))
@@ -256,38 +255,31 @@ class Fitting:
             _, stats_try = self._get_eigenvalues(
                 ypar+x, is_weighted=is_weighted
             )
-
             chi2_try, rms_try = stats_try[0], stats_try[1]
 
             if regular != 0.0:
                 chi2r = np.sum(np.square((c - np.dot(B, x))))
                 chi2_try = chi2_try + chi2r
-                rms_try = rms_try + math.sqrt(chi2r)
-
-            # eps = 1.0e-5
+                rms_try = rms_try + _sqrt(chi2r)
 
             # (5) Try to change the corrections
-            is_failed = False
-
-            if chi2_try < chi2_best:  # abs(chi2_try - chi2_best) < eps:
-                chi2_best = chi2_try
-                rms_best = rms_try
+            if chi2_try + eps < chi2_best:
+                chi2_best, rms_best = chi2_try, rms_try
                 ypar = ypar + x
+                is_failed = False
             else:  # chi2try >= chi2best
                 step = 10.0
                 ypar_try = np.copy(ypar)
                 x_try = np.copy(x)
                 x_try = x_try / step
+                is_failed = True
 
-                # try to improve the chi_square value
                 while True:
-                    # x_try = x_try / step
                     ypar_try = ypar_try + x_try
 
                     _, stats_new = self._get_eigenvalues(
                         ypar_try, is_weighted=is_weighted
                     )
-
                     chi2_new, rms_new = stats_new[0], stats_new[1]
 
                     if regular != 0.0:
@@ -295,29 +287,27 @@ class Fitting:
                             np.square(((yreg-ypar_try)) - np.dot(B, x_try))
                         )
                         chi2_new = chi2_new + chi2r
-                        rms_new = rms_new + math.sqrt(chi2r)
+                        rms_new = rms_new + _sqrt(chi2r)
 
-                    # if abs(chi2_new - chi2_best) < eps:
-                    if chi2_new < chi2_best:
-                        chi2_best = chi2_new
-                        rms_best = rms_new
+                    if chi2_new + eps < chi2_best:
+                        chi2_best, rms_best = chi2_new, rms_new
                         ypar = ypar_try
-
-                    else:  # chi2new >= chi2best:
-                        is_failed = True
-
-                        self.progress_str.write(
-                            f'\nCorrections changed =>\nChi square = '
-                            f'{chi2_best:.8f}\n'
-                            f'RMS = {rms_best:.8f}\n'
+                        info_corr = (
+                            f'Smaller step => Chi square = '
+                            f'{chi2_best:.6f} | RMS = {rms_best:.6f}'
                         )
-
+                        print(info_corr)
+                        self.progress_str.write(info_corr)
+                        is_failed = False
+                    else:  # chi2new >= chi2best:
                         break
 
-            # if it isn't got improved then change tol
-            if is_failed:
+            # on odd iteration try to change tol if nothing has been improved
+            if it > 1 and it % 2 == 1 and is_failed:
                 tol /= 5.0
-                self.progress_str.write(f'\nTOL CHANGED {"":>15}= {tol:.1e}')
+                info_tol = f'\nTol changed = {tol:.1e}'
+                print(info_tol)
+                self.progress_str.write(info_tol)
 
             # (6) Save the final parameters
             if self.progress:
@@ -328,13 +318,13 @@ class Fitting:
             if len(self.ml.couplings) > 0:
                 Coupling.edit_coupling_parameters(ypar, self.ml.couplings)
 
-            self.progress_str.write(
-                f'\nChi square final/best{"":>6}= {chi2_best:.8f} '
-                f'for iteration {it}'
-                f'\nRMS final{"":>18}= {rms_best:.8f} cm-1'
-            )
+            if self.progress:
+                print(self.progress_str.getvalue())
 
-            print(self.progress_str.getvalue())
+        print(
+            f'\nChi square final = {chi2_best:.6f} | '
+            f'RMS final = {rms_best:.6f} cm-1'
+        )
 
     def _generate_numerical_derivatives(self, ypar, yfixed, ycal_init,
                                         is_weighted, step_size):
@@ -359,64 +349,69 @@ class Fitting:
         dp[yfixed == 0] = 0.0
 
         for prm in range(0, ypar.shape[0]):
-            if yfixed[prm]:
-                dpar = np.copy(ypar)
-                dpar[prm] += dp[prm]
-                out_data, _ = \
-                    self._get_eigenvalues(dpar, is_weighted=is_weighted)
-                ycal = out_data[:, 7] / C_hartree
-                dydp[:, prm] = (ycal - ycal_init) / dp[prm]
+            if not yfixed[prm]:
+                continue
+            dpar = np.copy(ypar)
+            dpar[prm] += dp[prm]
+            out_data, _ = self._get_eigenvalues(dpar, is_weighted=is_weighted)
+            ycal = out_data[:, 7] / C_hartree
+            dydp[:, prm] = (ycal - ycal_init) / dp[prm]
 
         return dydp
 
-    def _generate_analytical_derivatives(self, ypar, yfixed,
-                                         qnums, is_weighted):
+    def _generate_analytical_derivatives(self, ypar, yfixed, qnums):
 
-        if not self.ml.sderiv:
-            raise SystemExit(
-                'Error: Spline derivatives should be calculated. '
-                'Change model to cspline.'
-            )
-
-        size = self.ml.nch*self.ml.ngrid
-        evec_selected = np.zeros((size, qnums.shape[0]))
+        msize = self.ml.nch*self.ml.ngrid
+        evecs_found = np.zeros((msize, qnums.shape[0]))
+        dydp = np.zeros((evecs_found.shape[1], ypar.shape[0]))
 
         for i in range(0, qnums.shape[0]):
-            evec_name = \
+            ename = \
                 f'evector_J{int(qnums[i, 1])}_' + \
                 f'p{int(qnums[i, 2])}_i{int(qnums[i, 3])}.npy'
+            evectors = np.load(_join('eigenvectors', ename))
+            vi = np.where(evectors[0, :].astype(int) == int(qnums[i, 0]))[0]
+            sti = np.where(evectors[1, :].astype(int) == int(qnums[i, 4]))[0]
+            evecs_found[:msize, i] = evectors[2:, np.intersect1d(vi, sti)[0]]
 
-            evectors = np.load(os.path.join('eigenvectors', evec_name))
-            vibnums = evectors[0, :].astype(np.int64)
-            states = evectors[1, :].astype(np.int64)
-            vi = np.where(vibnums == int(qnums[i, 0]))[0]
-            sti = np.where(states == int(qnums[i, 4]))[0]
-            evector = evectors[2:, np.intersect1d(vi, sti)[0]]
-            evec_selected[0:size, i] = evector
-
-        dydp = np.zeros((evec_selected.shape[1], ypar.shape[0]))
+        ejqnums, fjqnums = qnums[qnums[:, 2] == 1], qnums[qnums[:, 2] == 0]
+        fjqind = (fjqnums[:, 1] - np.min(fjqnums[:, 1])).astype(int)
+        ejqind = (ejqnums[:, 1] - np.min(ejqnums[:, 1])).astype(int)
+        ejqind += self.ml.jqnumbers.shape[0]
+        jqind = np.hstack((fjqind, ejqind))
 
         for prm in range(0, ypar.shape[0]):
-            if yfixed[prm]:
-                sk = np.zeros((size, size))
-                for ind in self.ml.block_index[prm]:
-                    r1, r2, c1, c2, count = ind
-                    if r1 == c1 and r2 == c2:
-                        # not working for non-diagonal block
-                        sk = sk + np.diag(self.ml.sk_grid[:, count])
-                    else:
-                        # i don't know why this is not working in both cases
-                        np.fill_diagonal(
-                            sk[r1:r2, c1:c2], self.ml.sk_grid[:, prm]
-                        )
-                        np.fill_diagonal(
-                            sk[c1:c2, r1:r2], self.ml.sk_grid[:, prm]
-                        )
-                        # sk = sk + sk.T  # <-- not working for diagonal block
-                enr_vec = np.diag(evec_selected.T @ sk @ evec_selected)
-                dydp[:, prm] = enr_vec
-
+            if not yfixed[prm]:
+                continue
+            sk = np.zeros((msize, msize))
+            for iprm in self.ml.block_index[prm]:
+                r1, r2, c1, c2, count, cp = iprm
+                sk_grid = self.ml.sk_grid[r1:r2, count]
+                if cp < 0:  # potential
+                    np.fill_diagonal(sk[r1:r2, c1:c2], sk_grid)
+                    dydp[:, prm] = np.diag(evecs_found.T @ sk @ evecs_found)
+                else:  # coupling
+                    sk_coef = self.ml.interact.cp_map[cp][r1:r2, :]
+                    memoize = set()
+                    for countj, j in enumerate(jqind):
+                        if j not in memoize:
+                            elem = self.get_dydp_elem(iprm[:4], sk, sk_grid,
+                                                      sk_coef, j, evecs_found)
+                        dydp[countj, prm] = elem[countj]
+                        memoize.add(j)
         return dydp
+
+    # @staticmethod
+    def get_dydp_elem(self, indices, sk, sk_grid, sk_coef, j, evecs_found):
+        r1, r2, c1, c2 = indices
+        np.fill_diagonal(sk[r1:r2, c1:c2], sk_grid)
+        sk[r1:r2, c1:c2] = sk[r1:r2, c1:c2] * sk_coef[:, j]
+        if r1 != c1 or r2 != c2:
+            np.fill_diagonal(sk[c1:c2, r1:r2], sk_grid)
+            sk[c1:c2, r1:r2] = sk[c1:c2, r1:r2] * sk_coef[:, j]
+        elem = np.diag(evecs_found.T @ sk @ evecs_found)
+
+        return elem
 
     @staticmethod
     def _find_corrections(A, b, tol, lapack_driver):
@@ -446,6 +441,31 @@ class Fitting:
 
         return x, rank, s
 
+    def check_derivative(self):
+
+        if not self.ml.is_cspline_pec and not self.ml.is_cspline_cpl:
+            _error(
+                'Change model parameter to cspline: analytical '
+                'derivatives can be calculated only for model=cspline'
+            )
+            raise SystemExit()
+
+        if not self.ml.is_cspline_pec:
+            _warning(
+                'The parameter model for the channel objects is not set '
+                'to cspline.\nThis will cause a failure (KeyError exception) '
+                'if you try to optimize the channel parameters.\n'
+                'Change model=cspline.\n'
+            )
+
+        if not self.ml.is_cspline_cpl:
+            _warning(
+                'The parameter model for the coupling objects is not set '
+                'to cspline.\nThis will cause a failure (KeyError exception) '
+                'if you try to optimize the coupling parameters.\n'
+                'Change model=cspline.\n'
+            )
+
     def _save_svd_parameters(self, rank, sv, tol, ypar, x, yfixed, it):
 
         # singular values are sorted from the largest to the smallest one
@@ -459,7 +479,7 @@ class Fitting:
         )
 
         self.progress_str.write(
-            ' '.join(['{:.1e}'.format(i) for i in sv[0:rank]])
+            ' '.join(['{:.1e}'.format(i) for i in sv[:rank]])
         )
 
         # the conditon number is the ratio of the largest to the
@@ -509,4 +529,4 @@ class Fitting:
             niter (int, optional): Number of iterations. Defaults to 1.
         """
 
-        pass
+        raise NotImplementedError()

diatomic/interaction.py
@@ -1,50 +1,59 @@
 from math import sqrt as _sqrt
 import numpy as np
-from .utils import C_hartree
+from utils import C_hartree
 
 __all__ = ['Interaction']
 
 
 class Interaction:
 
-    def __init__(self, ngrid, nch, rgrid2):
+    def __init__(self, ngrid, nch, ncp, rgrid2):
 
         self.ngrid = ngrid
         self.nch = nch
         self.rgrid2 = rgrid2
+        self.countl = 0
+        self.cp_map = {}
+        max_rotnj = 100
 
-    def get_perturbations_matrix(self, jrotn, mass, par, channels,
-                                 couplings, fgrid, dd, Gy2):
+        # TODO: the upper limit is not correct
+        for cp in range(0, ncp+6):
+            self.cp_map[cp] = np.zeros((self.nch * self.ngrid, max_rotnj))
 
-        self.fgrid = fgrid
+    def get_interactions_matrix(self, jrotn, mass, par, channels, couplings,
+                                fgrid, dd, Gy2):
+
         self.jrotn = jrotn
+        jjrotn = self.jrotn*(self.jrotn + 1.0)
 
-        self.pert_matrix = np.zeros((self.nch*self.ngrid)**2).reshape(
+        pert_matrix = np.zeros((self.nch*self.ngrid)**2).reshape(
             self.nch*self.ngrid, self.nch*self.ngrid
         )
 
-        for cp in range(0, len(couplings)):
+        interact_keys = self.define_interaction_keys()
 
-            # if isinstance(couplings[cp].interact[0], tuple):
-            props = zip(
+        for cp in range(0, len(couplings)):
+            cprops = zip(
                 couplings[cp].interact,
                 couplings[cp].coupling,
                 couplings[cp].multiplier
             )
 
-            for inter, ctype, m in props:
+            ycs = fgrid[cp*self.ngrid:(cp+1)*self.ngrid]
 
-                ch1, ch2 = inter[0:2]
-                cb = (ch1, ch2)
-
+            for countc, (inter, ctype, m) in enumerate(cprops):
+                ch1, ch2 = inter[:2]
                 args = self.get_quantum_numbers(channels, ch1, ch2)
+                cfunc = interact_keys[ctype](jjrotn, mass, m, par, args)  # Gy2
+                row1, row2 = (ch1-1)*self.ngrid, ch1*self.ngrid
+                col1, col2 = (ch2-1)*self.ngrid, ch2*self.ngrid
 
-                self.fill_pert_matrix(
-                    Gy2, ctype=ctype, m=m, cp=cp, cb=cb,
-                    mass=mass, par=par, dd=dd, args=args
-                )
+                pert_matrix[row1:row2, col1:col2][dd] += cfunc * ycs
+                self.cp_map[cp+countc][row1:row2, self.countl] = cfunc
 
-        return self.pert_matrix
+        self.countl += 1
+
+        return pert_matrix
 
     def get_quantum_numbers(self, channels, ch1, ch2):
 
@@ -77,36 +86,7 @@ class Interaction:
             'spin-spin': self.spin_spin_interaction,
         }
 
-    def fill_pert_matrix(self, Gy2, **kwargs):
-
-        ctype = kwargs['ctype']  # .lower()
-        m = kwargs['m']
-        cp = kwargs['cp']
-        cb = kwargs['cb']
-        mass = kwargs['mass']
-        par = kwargs['par']
-        dd = kwargs['dd']
-        args = kwargs['args']
-
-        interaction_keys = self.define_interaction_keys()
-
-        jjrotn = self.jrotn*(self.jrotn + 1.0)
-
-        i1 = cp*self.ngrid
-        i2 = (cp+1)*self.ngrid
-        ycs = self.fgrid[i1:i2]
-
-        cfunc = interaction_keys[ctype](jjrotn, mass, m, par, ycs, args)  # Gy2
-
-        ch1, ch2 = cb
-        row1 = (ch1-1)*self.ngrid
-        row2 = ch1*self.ngrid
-        col1 = (ch2-1)*self.ngrid
-        col2 = ch2*self.ngrid
-
-        self.pert_matrix[row1:row2, col1:col2][dd] += cfunc
-
-    def spin_orbit_interaction(self, jjrotn, mass, m, par, ycs, args):
+    def spin_orbit_interaction(self, jjrotn, mass, m, par, args):
 
         """
         Calculate diagonal and off-diagonal Spin-Orbit coupling matrix element
@@ -118,8 +98,9 @@ class Interaction:
         """
 
         socoef = m * (self.rule_SOdiag(args) or self.rule_SOnondiag(args))
+
         # return (ycs / C_hartree) * socoef
-        return ycs * socoef
+        return socoef
 
     def rule_SOdiag(self, args):
 
@@ -139,7 +120,7 @@ class Interaction:
 
         return 0
 
-    def lj_interaction(self, jjrotn, mass, m, par, ycs, args):
+    def lj_interaction(self, jjrotn, mass, m, par, args):
 
         """
         Calculate the matrix element of L-uncoupling operator
@@ -160,7 +141,7 @@ class Interaction:
 
         sign = -1.0
 
-        return sign * ycs * brot * ljcoef
+        return sign * brot * ljcoef
 
     def rule_lj_interaction(self, args):
 
@@ -197,7 +178,7 @@ class Interaction:
 
         return 0
 
-    def lj_parity_interaction(self, jjrotn, mass, m, par, ycs, args):
+    def lj_parity_interaction(self, jjrotn, mass, m, par, args):
 
         qexpression = _sqrt(jjrotn + args['om1'] * args['om2'])
 
@@ -209,7 +190,7 @@ class Interaction:
         if par == 1:
             sign = -1.0  # e-parity
 
-        return sign * ycs * brot * ljcoef
+        return sign * brot * ljcoef
 
     def rule_lj_parity(self, args):
 
@@ -228,7 +209,7 @@ class Interaction:
 
         return 0
 
-    def sj_interaction(self, jjrotn, mass, m, par, ycs, args):
+    def sj_interaction(self, jjrotn, mass, m, par, args):
 
         """
         Calculate the matrix element of S-uncoupling operator
@@ -253,7 +234,7 @@ class Interaction:
 
         sign = -1.0
 
-        return sign * ycs * brot * sjcoef
+        return sign * brot * sjcoef
 
     def rule_sj_interaction(self, args):
 
@@ -270,7 +251,7 @@ class Interaction:
 
         return 0
 
-    def sj_parity_interaction(self, jjrotn, mass, m, par, ycs, args):
+    def sj_parity_interaction(self, jjrotn, mass, m, par, args):
         """
             only between Sigma states
         """
@@ -285,7 +266,7 @@ class Interaction:
         if par == 1:
             sign = -1.0  # e-parity
 
-        return sign * ycs * brot * sjcoef
+        return sign * brot * sjcoef
 
     def rule_sj_parity(self, args):
 
@@ -294,7 +275,7 @@ class Interaction:
 
         return 0
 
-    def spin_electornic_interaction(self, jjrotn, mass, m, par, ycs, args):
+    def spin_electornic_interaction(self, jjrotn, mass, m, par, args):
         """
         Calculate the matrix element of spin-electronic operator
         <State1| LS |State1> =
@@ -313,7 +294,7 @@ class Interaction:
 
         sign = 1.0
 
-        return sign * ycs * brot * slcoef
+        return sign * brot * slcoef
 
     def rule_spin_electronic(self, args):
 
@@ -329,37 +310,37 @@ class Interaction:
 
         return 0
 
-    def lambda_doubling(self, jjrotn, mass, m, par, ycs, args):
+    def lambda_doubling(self, jjrotn, mass, m, par, args):
 
         ldcoef = jjrotn / (2.0 * mass * self.rgrid2)**2
 
-        return ycs * m * ldcoef * C_hartree
+        return m * ldcoef
 
-    def lambda_doubling_e_parity(self, jjrotn, mass, m, par, ycs, args):
+    def lambda_doubling_e_parity(self, jjrotn, mass, m, par, args):
 
         if par == 1:
-            return self.lambda_doubling(jjrotn, mass, m, par, ycs, args)
-        else:
-            return np.zeros(ycs.shape[0])
+            return self.lambda_doubling(jjrotn, mass, m, par, args)
 
-    def lambda_doubling_f_parity(self, jjrotn, mass, m, par, ycs, args):
+        return np.zeros(self.rgrid2.shape[0])
+
+    def lambda_doubling_f_parity(self, jjrotn, mass, m, par, args):
 
         if par == 0:
-            return self.lambda_doubling(jjrotn, mass, m, par, ycs, args)
-        else:
-            return np.zeros(ycs.shape[0])
+            return self.lambda_doubling(jjrotn, mass, m, par, args)
 
-    def NDBOBC(self, jjrotn, mass, m, par, ycs, args):
+        return np.zeros(self.rgrid2.shape[0])
+
+    def NDBOBC(self, jjrotn, mass, m, par, args):
 
         bocoef = jjrotn / (2.0 * mass * self.rgrid2)
 
-        return ycs * m * bocoef * C_hartree
+        return m * bocoef * C_hartree
 
-    def DBOBC(self, jjrotn, mass, m, par, ycs, args):
+    def DBOBC(self, jjrotn, mass, m, par, args):
 
-        return (ycs / C_hartree) * m
+        return (1.0 / C_hartree) * m
 
-    def spin_rotation_interaction(self, jjrotn, mass, m, par, ycs, args):
+    def spin_rotation_interaction(self, jjrotn, mass, m, par, args):
 
         if self.rule_spin_rot_diag(args):
 
@@ -375,7 +356,7 @@ class Interaction:
 
         sign = 1.0
 
-        return sign * ycs * srcoef
+        return sign * srcoef
 
     def rule_spin_rot_diag(self, args):
 
@@ -391,7 +372,7 @@ class Interaction:
 
         self.rule_sj_interaction(args)
 
-    def spin_spin_interaction(self, jjrotn, mass, m, par, ycs, args):
+    def spin_spin_interaction(self, jjrotn, mass, m, par, args):
 
         if self.rule_spin_spin_diag(args):
             qexpression = 3 * args['sg1']**2 - args['s1']*(args['s2'] + 1.0)
@@ -403,7 +384,7 @@ class Interaction:
 
         sign = 1.0
 
-        return sign * ycs * sscoef
+        return sign * sscoef
 
     def rule_spin_spin_nondiag(self, args):
 

diatomic/molecule_data.py
@@ -3,10 +3,10 @@ from os.path import exists as _exists, splitext as _splitext
 from logging import error as _error, warning as _warning
 from io import open as _open
 from os import stat as _stat
-from collections import OrderedDict
 import numpy as np
-from .data.atomic_database import AtomicDatabase
-from .utils import C_hartree, C_bohr, C_massau
+from collections import OrderedDict
+from data.atomic_database import AtomicDatabase
+from utils import C_hartree, C_bohr, C_massau
 
 # to check if C loader is present on current machine
 # /path/to/python -c "import ruamel.yaml;
@@ -353,7 +353,7 @@ class Channel:
         cls.models = cls._define_channel_models()
         cls.channels = channels
         cls.unique_channels = OrderedDict()
-        cls.totp, cls.tot_punique = 0, 0
+        cls.totp, cls.totp_unique = 0, 0
 
         for ci, ch in enumerate(channels):
 
@@ -387,7 +387,7 @@ class Channel:
                 cls.unique_channels[ch.filep] = (ci, ch)
                 ch.pointer = (ci, ch)
                 ch.isunique = 1
-                cls.tot_punique += ch.npnts
+                cls.totp_unique += ch.npnts
             else:
                 ch.pointer = cls.unique_channels[ch.filep]
                 ch.isunique = 0
@@ -756,6 +756,10 @@ class Coupling:
             if 'spin-orbit' in cp.coupling:
                 cp.yunits = 1.0 / C_hartree
 
+            if all(item.startswith('lambdad') for item in cp.coupling):
+                cp.yunits = C_hartree
+
+            # set parameters for pointwise models
             if cp.model == cls.models[1] or cp.model == cls.models[2]:
                 cp.xc = params[:, 0] * cp.xunits
                 cp.yc = params[:, 1] * cp.yunits
@@ -770,6 +774,7 @@ class Coupling:
                     cls.cregular = np.append(cls.cregular, cp.regularp)
                     cls.clambda = np.append(cls.clambda, cp.lambdai)
 
+            # set parameters for constant or custom models
             if cp.model == cls.models[3] or cp.model == cls.models[4]:
                 cp.yc = params[:, 0]
 
@@ -790,6 +795,7 @@ class Coupling:
                 try:
                     return yaml.load(inps)
                 except yaml.YAMLError as exc:
+                    # TODO: fails with YAML object has no attribute YAMLError
                     raise SystemExit(exc)
         except IOError as e:
             raise SystemExit(e)

diatomic/molecule_levels.py
@@ -4,10 +4,10 @@ from os.path import join as _join
 import numpy as np
 import scipy as sp
 from scipy.interpolate import CubicSpline as _CubicSpline
-from .interaction import Interaction
-from .molecule_data import Channel, Coupling
-from .grids import CSpline
-from .utils import Utils, C_hartree, C_bohr, C_massau
+from interaction import Interaction
+from molecule_data import Channel, Coupling
+from grids import CSpline
+from utils import Utils, C_hartree, C_bohr, C_massau
 # from numba import njit
 
 __all__ = ['MoleculeLevels']
@@ -18,7 +18,7 @@ class MoleculeLevels:
     def __init__(self, md, grid):
 
         self.jqnumbers = md.jqnumbers
-        self.pars = md.parities
+        self.pars = sorted(md.parities)
         self.masses = md.reduced_masses or md.masses
         self.nisotopes = md.nisotopes
         self.refj = md.referencej
@@ -27,6 +27,7 @@ class MoleculeLevels:
 
         self.channels = Channel.get_channel_objects()
         self.nch = len(self.channels)
+
         self.couplings = []
         self.ncp = 0
         try:
@@ -59,20 +60,25 @@ class MoleculeLevels:
         self.solver_keys = self._get_solver_keys()
 
         self.rgrid2 = np.square(self.rgrid)
-        self.ugrid = np.zeros(self.nch * self.ngrid)
+        msize = self.nch * self.ngrid
+        self.ugrid = np.zeros(msize)
         self.fgrid = np.zeros(self.ncp * self.ngrid)
 
-        self.sderiv = False  # spline derivatives
+        # is analytical derivative for pec allowed
+        self.is_cspline_pec = False
+        # is analytical derivative for couplings allowed
+        self.is_cspline_cpl = False
+        # map potential/channel parameters to hmatrix block indices
         self.block_index = {}
-
+        self.interact = None
         nmax_params = 200  # maximum number of fit parameters
-        self.sk_grid = np.zeros((self.nch * self.ngrid, nmax_params))
+        self.sk_grid = np.zeros((msize, nmax_params))
 
-        self.pot_enr = np.zeros((self.nch*self.ngrid, self.nch*self.ngrid))
+        self.pot_enr = np.zeros((msize, msize))
         self.kin_enr = np.zeros_like(self.pot_enr)
 
         self.evals_file = 'eigenvalues.dat'
-        self.predicted_file = 'evalues_predicted.dat'
+        self.predicted_file = 'eigenvalues_predicted.dat'
         self.info_outfile = 'data_info.dat'
         self.evec_dir = _join(Utils.get_current_dir(), 'eigenvectors')
         self.wavef_dir = _join(Utils.get_current_dir(), 'wavefunctions')
@@ -120,7 +126,7 @@ class MoleculeLevels:
             if self.channels[ch-1].model.upper() == Channel.models[5]:
                 self._calculate_MLR_pec_on_grid(ch, ypar, ci)
 
-            # custom potential function
+            # custom (user-specified) potential function
             if self.channels[ch-1].model.lower() == Channel.models[6]:
                 self._calculate_custom_pec_on_grid(ch, ypar, ci)
 
@@ -153,16 +159,17 @@ class MoleculeLevels:
         self.ugrid[index1:index2], sk = cs(self.rgrid, return_deriv=True)
 
         for pkey in range(ci[0], ci[1]):
-            indices = (index1, index2, index1, index2, counter[0])
+            indices = (index1, index2, index1, index2, counter[0], -1)
             if pkey not in self.block_index:
                 self.block_index[pkey] = [indices]
             else:
                 self.block_index[pkey].append(indices)
             counter[0] += 1
+
         ci[0] = ci[1]
         stp, enp = (ch-1)*npnts, ch*npnts
         self.sk_grid[(ch-1)*self.ngrid:ch*self.ngrid, stp:enp] = sk
-        self.sderiv = True
+        self.is_cspline_pec = True
 
     def _calculate_Morse_pec_on_grid(self, ch, ypar, ci):
 
@@ -199,9 +206,10 @@ class MoleculeLevels:
         nc = self.channels[ch-1].nc
         nd = self.channels[ch-1].nd
 
+        nall = (ni, nb, nc, nd)
+
         ypnts = self.channels[ch-1].U
-        self.ugrid[(ch-1)*self.ngrid:ch*self.ngrid] = \
-            self._VMLR(ypnts, (ni, nb, nc, nd))
+        self.ugrid[(ch-1)*self.ngrid:ch*self.ngrid] = self._VMLR(ypnts, nall)
 
     def _VMLR(self, params, nparams):
 
@@ -260,7 +268,7 @@ class MoleculeLevels:
     def calculate_couplings_on_grid(self, ypar=None):
 
         # pass by reference
-        yrange = {'start': Channel.tot_punique, 'end': Channel.tot_punique}
+        yrange = {'start': Channel.totp_unique, 'end': Channel.totp_unique}
         self._countp = Channel.totp
 
         for cp in range(0, self.ncp):
@@ -296,29 +304,34 @@ class MoleculeLevels:
     def _calculate_cspline_coupling_on_grid(self, cp, yrange, ypar):
 
         xpnts = self.couplings[cp].xc
-        # stp = yrange['start']
+        stp = yrange['start']
 
         # will change yrange
         ypnts = self.calculate_coupling_points(cp, yrange, ypar)
-        # enp = yrange['end']
+        enp = yrange['end']
 
         cs = CSpline(xpnts, ypnts)
         index = cp*self.ngrid, (cp+1)*self.ngrid
 
         self.fgrid[index[0]:index[1]], sk = cs(self.rgrid, return_deriv=True)
 
-        for pair in self.couplings[cp].interact:
+        for countp, pair in enumerate(self.couplings[cp].interact):
             stpr = self._countp
             enpr = stpr + self.couplings[cp].npnts
             ch1, ch2 = pair
             row1, row2 = (ch1-1)*self.ngrid, ch1*self.ngrid
             col1, col2 = (ch2-1)*self.ngrid, ch2*self.ngrid
 
-            self.block_index.update(
-                {pkey: [row1, row2, col1, col2] for pkey in range(stpr, enpr)}
-            )
+            for pkey, count in zip(range(stp, enp), range(stpr, enpr)):
+                indices = (row1, row2, col1, col2, count, cp+countp)
+                if pkey not in self.block_index:
+                    self.block_index[pkey] = [indices]
+                else:
+                    self.block_index[pkey].append(indices)
+
             self.sk_grid[row1:row2, stpr:enpr] = sk
-        self.sderiv = True
+
+        self.is_cspline_cpl = True
 
     def calculate_coupling_points(self, cp, yrange, ypar):
 
@@ -330,7 +343,7 @@ class MoleculeLevels:
 
     def calculate_constant_coupling_on_grid(self, cp, ypar):
 
-        st = Channel.tot_punique
+        st = Channel.totp_unique
         en = st + self.couplings[cp].npnts
         ygrid = ypar[st:en]
         st += self.couplings[cp].npnts
@@ -399,14 +412,15 @@ class MoleculeLevels:
             2: 'energy_subset_index',
             3: 'energy_subset_value',
             4: 'identify',
+            6: 'store_predicted',
             7: 'store_evecs',
             8: 'weighted',
             11: 'lapack_driver',
             12: 'arpack_which',
             13: 'arpack_k',
             14: 'arpack_sigma',
-            15: 'file_evals_predicted',
-            16: 'file_eigenvalues',
+            15: 'eigp_file',
+            16: 'eigc_file',
         }
 
     def calculate_levels(self, ypar=None, **kwargs):
@@ -419,6 +433,7 @@ class MoleculeLevels:
         self.energy_subset_index = kwargs.get(keys[2])
         self.energy_subset_value = kwargs.get(keys[3])
         self.identify = kwargs.get(keys[4]) or 0
+        self.store_predicted = kwargs.get(keys[6]) or False
         self.store_evecs = kwargs.get(keys[7]) or False
         self.is_weighted = kwargs.get(keys[8]) or False
         self.file_predicted = kwargs.get(keys[15]) or self.predicted_file
@@ -451,7 +466,12 @@ class MoleculeLevels:
         if len(self.couplings) > 0:
             ypar = np.hstack((Channel.ppar, Coupling.cpar))
 
-        chi2, rms, _ = self.calculate_eigenvalues(ypar)
+        chi2, rms, rmsd = self.calculate_eigenvalues(ypar)
+
+        self._save_output_data(stats=(chi2, rms, rmsd))
+
+        if self.store_predicted:
+            self.save_predicted()
 
         print(f'Chi Square = {chi2:<18.8f} | RMS = {rms:<15.8f}cm-1')
         # f'\nRMSD = {rmsd:.8f}'
@@ -463,15 +483,18 @@ class MoleculeLevels:
         if self.ncp != 0:
             self.calculate_couplings_on_grid(ypar=ypar)
 
-        interact = Interaction(self.ngrid, self.nch, self.rgrid2)
+        self.interact = Interaction(
+            self.ngrid, self.nch, self.ncp, self.rgrid2
+        )
         dd = np.diag_indices(self.ngrid)
         count = 0
 
         for niso, iso in enumerate(self.nisotopes):
-
-            self.evals_predicted = np.zeros(
-                (2 * self.nch * self.exp_data.shape[0], 6+self.nch)
-            )
+            minxdim = 1000
+            xdim = 2 * self.nch * self.exp_data.shape[0]
+            if xdim < minxdim:
+                xdim = minxdim
+            self.evals_predicted = np.zeros((xdim, 6+self.nch))
 
             mass = self.masses[iso-1]
 
@@ -487,7 +510,7 @@ class MoleculeLevels:
 
                     self.hmatrix = self.kin_enr + self.pot_enr
 
-                    pert_matrix = interact.get_perturbations_matrix(
+                    pert_matrix = self.interact.get_interactions_matrix(
                         jrotn, mass, par, self.channels, self.couplings,
                         self.fgrid, dd, self.Gy2
                     )
@@ -504,7 +527,7 @@ class MoleculeLevels:
                     if self.store_evecs:
 
                         self._save_eigenvectors(
-                            jrotn, par, iso, evecs[:, 0:evalues.shape[0]],
+                            jrotn, par, iso, evecs[:, :evalues.shape[0]],
                             vibnums, states
                         )
 
@@ -517,7 +540,7 @@ class MoleculeLevels:
                     ).reshape(evalues.shape[0], -1)
 
                     nrow, ncol = calc_data.shape[0], calc_data.shape[1]
-                    self.evals_predicted[count:count+nrow, 0:ncol] = calc_data
+                    self.evals_predicted[count:count+nrow, :ncol] = calc_data
 
                     count += calc_data.shape[0]
 
@@ -544,7 +567,8 @@ class MoleculeLevels:
             self.out_data[:, 10], self.is_weighted
         )
 
-        self._save_output_data(stats=(chi2, rms, rmsd))
+        # the sorting is needed for the analytical derivative
+        self._sort_output(cols=[5, 1])
 
         return chi2, rms, rmsd
 
@@ -593,7 +617,7 @@ class MoleculeLevels:
         ml = 4.0 * mass * length**2
         h2 = h**2
 
-        ij_grid = np.mgrid[0:self.ngrid, 0:self.ngrid]
+        ij_grid = np.mgrid[:self.ngrid, :self.ngrid]
         ij_mtx = ij_grid[0] - ij_grid[1]
 
         di = np.diag_indices(self.ngrid)
@@ -601,7 +625,7 @@ class MoleculeLevels:
         sinf_mtx = np.sin((ij_mtx * np.pi) / self.ngrid)
         sinf_mtx[di] = 1.0  # set diagonal to some temporary nonzero value
 
-        self.kin_enr[0:self.ngrid, 0:self.ngrid] = \
+        self.kin_enr[:self.ngrid, :self.ngrid] = \
             (np.power(-1.0, ij_mtx) * h2) / (ml * np.power(sinf_mtx, 2))
         self.kin_enr[di] = (h2 / ml) * n2
 
@@ -610,7 +634,7 @@ class MoleculeLevels:
             ind2 = ch*self.ngrid
 
             self.kin_enr[ind1:ind2, ind1:ind2] = \
-                self.kin_enr[0:self.ngrid, 0:self.ngrid]
+                self.kin_enr[:self.ngrid, :self.ngrid]
 
     def _calculate_kinetic_energy_sinc(self, mass):
 
@@ -619,7 +643,7 @@ class MoleculeLevels:
         h = (self.rmax - self.rmin) / (self.ngrid-1)
         h2 = (pc ** 2) / (2.0 * mass)
 
-        ij_grid = np.mgrid[0:self.ngrid, 0:self.ngrid]
+        ij_grid = np.mgrid[:self.ngrid, :self.ngrid]
         ij_diff = ij_grid[0] - ij_grid[1]
         ij_sum = ij_grid[0] + ij_grid[1]
         di = np.diag_indices(self.ngrid)
@@ -627,7 +651,7 @@ class MoleculeLevels:
         # set diagonal to some temporary nonzero value
         ij_diff[di] = 1.0
 
-        self.kin_enr[0:self.ngrid, 0:self.ngrid] = \
+        self.kin_enr[:self.ngrid, :self.ngrid] = \
             (2.0 * np.power(-1.0, ij_sum)) / np.power(ij_diff, 2)
         self.kin_enr[di] = pi2 / 3.0
         self.kin_enr = self.kin_enr * (h2 / h**2)
@@ -637,7 +661,7 @@ class MoleculeLevels:
             ind4 = ch*self.ngrid
 
             self.kin_enr[ind3:ind4, ind3:ind4] = \
-                self.kin_enr[0:self.ngrid, 0:self.ngrid]
+                self.kin_enr[:self.ngrid, :self.ngrid]
 
         self.kin_enr[di] = self.kin_enr[di] - (h2 * self.Fy)
 
@@ -657,7 +681,7 @@ class MoleculeLevels:
         d2 = np.empty(self.ngrid-2)
         d2.fill(1.0/12.0)
 
-        self.kin_enr[0:self.ngrid, 0:self.ngrid] = \
+        self.kin_enr[:self.ngrid, :self.ngrid] = \
             (h2/(step**2)) * self._form_five_diagonal_symm_matrix(d0, d1, d2)
 
         corner_coef = 29.0 / 12.0
@@ -669,7 +693,7 @@ class MoleculeLevels:
             ind4 = ch*self.ngrid
 
             self.kin_enr[ind3:ind4, ind3:ind4] = \
-                self.kin_enr[0:self.ngrid, 0:self.ngrid]
+                self.kin_enr[:self.ngrid, :self.ngrid]
 
     def _form_five_diagonal_symm_matrix(self, a, b, c, k1=0, k2=1, k3=2):
 
@@ -1276,25 +1300,23 @@ class MoleculeLevels:
             self.file_predicted, eigv_data, comments='#', header=header, fmt=fm
         )
 
-    def sort_predicted(self, cols=[]):
+    def _sort_predicted(self, cols=[]):
 
         out = []
         for col in cols:
             out.append(self.out_data[:, col])
-        out = tuple(out)
 
-        self.evals_predicted = self.evals_predicted[np.lexsort(out)]
-        self.save_predicted()
+        self.evals_predicted = self.evals_predicted[np.lexsort(tuple(out))]
+        # self.save_predicted()
 
-    def sort_output(self, cols=[]):
+    def _sort_output(self, cols=[]):
 
         out = []
-        for col in cols:
+        for col in reversed(cols):
             out.append(self.out_data[:, col])
-        out = tuple(out)
 
-        self.out_data = self.out_data[np.lexsort(out)]
-        self._save_output_data()
+        self.out_data = self.out_data[np.lexsort(tuple(out))]
+        # self._save_output_data()
 
     def _group_increasing_sequences(self, iseq):
 