398b31a8597f17f30bcb33e3303ea04f01922ef8
ihavalyova <havaliova@gmail.com>
2022-10-29 22:55:24 +0300

    adding an automatic build for the c++ code

--- setup.py
+++ setup.py
@@ -1,75 +1,88 @@
 #!/usr/bin/env python
 
-import io
+# Procedure for local instalation:
+# conda create --name diatom  # create new env
+# conda activate diatom (or source activate diatom) # actvate env
+# conda install numpy, scipy, matplotlib
+# pip install pybind11
+# python setup.py install
+
 import os
-from setuptools import setup, find_packages
 
-
-name = 'diatomic-computations'
-description = 'Computations of Diatomic Molecules.'
-url = 'https://github.com/ihavalyova/DiAtomic'
-email = 'havaliova@gmail.com'
-author = 'Ilvie Havalyova'
-requires_python = '>=3.6.0'
-version = 'v0.0.1-beta1'
-required_install = [
-    'numpy>=1.16.0',
-    'scipy>=1.5.0',
-    'matplotlib>=3.2.0',
-    'ruamel.yaml',
-]
-required_extras = {
-    'iminuit': ['iminuit'],
-    'py3nj': ['py3nj']
-}
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
 
 here = os.path.abspath(os.path.dirname(__file__))
 readme = os.path.join(here, 'README.md')
 
-try:
-    with io.open(readme, encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = description
+
+class get_pybind11_include_dirs(object):
+    """Returns a list of include directories for pybind11;
+    Ensure that pybind11 is installed before referencing it."""
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include()
 
 
-def run_setup():
+os.environ['CC'] = 'g++'
 
-    setup(
-        name=name,
-        version=version,
-        description=description,
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        author=author,
-        author_email=email,
-        python_requires=requires_python,
-        url=url,
-        download_url='',
-        keywords=[
-            'physics', 'diatomic-molecule', 'coupled-channels',
-            'energy-levels', 'deperturbation',
-        ],
-        packages=find_packages(
-            exclude=['tests', '*.tests', 'test*']
-        ),
-        install_requires=required_install,
-        extras_require=required_extras,
-        package_data={
-            '': []
-        },
-        include_package_data=True,
-        license='BSD License',
-        classifiers=[
-            "License :: OSI Approved :: BSD License",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-        ],
-        zip_safe=False,
-    )
+diatomic_ext1 = Extension(
+    "diatomic.identify",
+    sources=["diatomic/identify.cpp"],
+    extra_compile_args=['-O3', '-shared', '-std=c++11'],
+    include_dirs=[get_pybind11_include_dirs()]
+)
 
+diatomic_ext2 = Extension(
+    "diatomic.wavenumbers",
+    sources=["diatomic/wavenumbers.cpp"],
+    extra_compile_args=['-O3', '-shared', '-std=c++11'],
+    include_dirs=[get_pybind11_include_dirs()]
+)
 
-if __name__ == '__main__':
-    run_setup()
+setup(
+    name='diatomic-computations',
+    version='v0.0.1',
+    description='Diatomic Molecules Computations',
+    long_description=open(readme).read(),
+    long_description_content_type='text/markdown',
+    author='Ilvie Havalyova',
+    author_email='havaliova@gmail.com',
+    python_requires='>=3.7.0',
+    url='https://github.com/ihavalyova/DiAtomic',
+    download_url='',
+    license='BSD3',
+    keywords=[
+        'physics',
+        'diatomic-molecule',
+        'coupled-channels',
+        'energy-levels',
+        'deperturbation',
+    ],
+    packages=['diatomic'],
+    install_requires=[
+        'numpy>=1.16.0',
+        'scipy>=1.5.0',
+        'matplotlib>=3.2.0',
+    ],
+    extras_require={
+        'iminuit': ['iminuit'],
+        'py3nj': ['py3nj']
+    },
+    package_data={
+        'diatomic': ['identify.cpp', 'wavenumbers.cpp']
+    },
+    include_package_data=True,
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    zip_safe=False,
+    ext_modules=[diatomic_ext1, diatomic_ext2],
+)
